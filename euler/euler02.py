719a85586f0da610dcc4e821260a981c0d96aaa0
Adam CRIPPS <Adam.Cripps@saint-gobain.com>
2024-05-24 14:03:56 +0100

    Fibonacci sum of evens

---
+++ euler/euler02.py
@@ -0,0 +1,10 @@
+# fibonacci sequence
+
+def fibonacci(n):
+    a, b = 0, 1
+    while a < n:
+        yield a
+        a, b = b, a + b
+
+a = sum ( x for x in list(fibonacci(100000)) if x % 2 == 0)
+print(a)
